f298f5be3f8316d904cc9553b9931e59b66df22e
Laura Illouz <laura.hortense@gmail.com>
2021-07-06 14:46:36 +0200

    Exercise 3

--- chapitre_01/authors/server.js
+++ chapitre_01/authors/server.js
@@ -3,19 +3,24 @@ const app = express();
 
 var authors = require("./authors.js");
 
-// Route homepage
+// Exercie 1 - Route homepage
 app.get("/", (req, res) => {
     res.send("Authors API");
 });
 
-// Routes
-app.get("/authors/:authorId", (req, res) => {
+// Exercice 2 - Routes auteurs
+app.get("/authors/:authorId/", (req, res) => {
     let authorId = req.params.authorId;
     let author = authors[authorId -1];
     res.send(`${author.name}, ${author.nationality}`);
 });
 
-
+// Exercice 3 - Routes livres
+app.get("/authors/:authorId/books/", (req, res) => {
+    let authorId = req.params.authorId;
+    let author = authors[authorId -1];
+    res.send(`${author.books}`);
+});
 
 
 
