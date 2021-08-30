2d48b6e9463345c8b989ad8076fa4be2e5fb7a5b
Laura Illouz <laura.hortense@gmail.com>
2021-08-30 13:57:21 +0200

    Routes working (exercise 1 & 2)

--- chapitre_02/students/server.js
+++ chapitre_02/students/server.js
@@ -2,6 +2,32 @@ const express = require("express");
 const app = express();
 // Port
 const PORT = 3000;
+// Data Students
+const students = [{ name: "Laura" }];
+
+app.use(express.json());
+
+// Middleware global
+app.use((req, res, next) => {
+  console.log("Yas");
+  next();
+});
+
+//Routes
+app.get("/students", (req, res) => {
+  res.json({
+    students,
+  });
+});
+
+app.post("/students", (req, res) => {
+  const newStudent = req.body;
+  console.log(newStudent);
+  res.json({
+    message: "Student added",
+    newStudent,
+  });
+});
 
 // Server listening
 app.listen(PORT, () => {
