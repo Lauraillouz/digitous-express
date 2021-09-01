693952eaae93c5c22cd09dd521a061a7eff197a0
Laura Illouz <laura.hortense@gmail.com>
2021-09-01 12:18:38 +0200

    server set up

--- chapitre_03/server.js
+++ chapitre_03/server.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const morgan = require("morgan");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const PORT = 3000;
 const app = express();
 const upload = multer({ dest: "public/uploads" });
 
 // Middlewares
+app.use(express.json());
 app.use(morgan("tiny"));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+// Routes
+app.get("/", (_req, res) => {
+  res.json({
+    status: "OK",
+  });
+});
+
+app.post("/user", upload.single("image"), (req, res) => {
+  let username = req.query;
+  let profilePic = req.file;
+  console.log(username);
+  console.log(profilePic);
+  /* fs.renameSync(
+    req.file.path,
+    path.join(req.file.destination, req.file.originalname)
+  ); */
   res.json({
     status: "OK",
   });
