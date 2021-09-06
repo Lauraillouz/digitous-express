4efd181b0e82fb868baeba089de0c410d9c1f7ef
Laura Illouz <laura.hortense@gmail.com>
2021-09-06 14:05:07 +0200

    validator OK

--- trippy_api/routers/hotelsRouter.js
+++ trippy_api/routers/hotelsRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+// Libraries
+const expressValidator = require("express-validator");
 // Controllers
 const hotelsControllers = require("../controllers/hotelsController");
 
@@ -10,7 +11,33 @@ router.get("/stars", hotelsControllers.getHotelByStars);
 router.get("/:id", hotelsControllers.getHotelById);
 
 // POST
-router.post("/", hotelsControllers.newHotel);
+router.post(
+  "/",
+  expressValidator.body("name").isString().withMessage("Must be a string"),
+  expressValidator.body("address").isString().withMessage("Must be a string"),
+  expressValidator.body("city").isString().withMessage("Must be a string"),
+  expressValidator.body("country").isString().withMessage("Must be a string"),
+  expressValidator
+    .body("stars")
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Must be a number between 1 and 5"),
+  expressValidator.body("hasSpa").isBoolean().withMessage("Must be a boolean"),
+  expressValidator.body("hasPool").isBoolean().withMessage("Must be a boolean"),
+  expressValidator
+    .body("priceCategory")
+    .isInt({ min: 1, max: 3 })
+    .withMessage("Must be a number between 1 and 3"),
+  (req, res) => {
+    const errors = expressValidator.validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({
+        status: "Error",
+        message: errors,
+      });
+    }
+  },
+  hotelsControllers.newHotel
+);
 
 // PUT
 router.put("/:id", hotelsControllers.changeHotelName);
--- trippy_api/routers/restaurantsRouter.js
+++ trippy_api/routers/restaurantsRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+// Libraries
+const expressValidator = require("express-validator");
 // Controllers
 const restaurantsController = require("../controllers/restaurantsController");
 
@@ -9,7 +10,32 @@ router.get("/", restaurantsController.getRestaurants);
 router.get("/:id", restaurantsController.getRestaurantById);
 
 // POST
-router.post("/", restaurantsController.newRestaurant);
+router.post(
+  "/",
+  expressValidator.body("name").isString().withMessage("Must be a string"),
+  expressValidator.body("address").isString().withMessage("Must be a string"),
+  expressValidator.body("city").isString().withMessage("Must be a string"),
+  expressValidator.body("country").isString().withMessage("Must be a string"),
+  expressValidator
+    .body("stars")
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Must be a number between 1 and 5"),
+  expressValidator.body("cuisine").isString().withMessage("Must be a string"),
+  expressValidator
+    .body("priceCategory")
+    .isInt({ min: 1, max: 3 })
+    .withMessage("Must be a number between 1 and 3"),
+  (req, res) => {
+    const errors = expressValidator.validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({
+        status: "Error",
+        message: errors,
+      });
+    }
+  },
+  restaurantsController.newRestaurant
+);
 
 // PUT
 router.put("/:id", restaurantsController.changeRestaurantName);
