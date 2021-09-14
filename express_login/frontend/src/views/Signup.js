d7cf16e45a3fcc24a6485596a073ce71dc3cba91
Laura Illouz <laura.hortense@gmail.com>
2021-09-14 13:44:57 +0200

    Signup page setup OK

--- express_login/frontend/src/views/Signup.js
+++ express_login/frontend/src/views/Signup.js
@@ -1,7 +1,33 @@
 import React from "react";
+// CSS
+import "../global.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <form className="flexForm width">
+        <label>First name</label>
+        <input type="text" />
+
+        <label>Surname</label>
+        <input type="text" />
+
+        <label>Date of birth</label>
+        <input type="date" />
+
+        <label>Email</label>
+        <input type="email" />
+
+        <label>Password</label>
+        <input type="password" />
+
+        <label>Confirm password</label>
+        <input type="password" />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
