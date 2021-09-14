e3769e3165ac1087a5830e745410ae7824973b1b
Laura Illouz <laura.hortense@gmail.com>
2021-09-14 13:50:52 +0200

    Login setup OK

--- express_login/frontend/src/views/Login.js
+++ express_login/frontend/src/views/Login.js
@@ -1,7 +1,19 @@
 import React from "react";
+// CSS
+import "../global.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <form className="flexForm width">
+      <label>Email</label>
+      <input type="email" />
+
+      <label>Password</label>
+      <input type="password" />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default Login;
